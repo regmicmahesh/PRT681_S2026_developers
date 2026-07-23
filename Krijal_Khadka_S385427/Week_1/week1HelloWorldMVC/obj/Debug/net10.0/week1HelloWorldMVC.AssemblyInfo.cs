16771356dd6d05222ba25f9869549be812bd7970
khadkakrijal <khadkakrijal36@gmail.com>
2026-07-23 15:42:05 +0930

    add  Helloworld controller

--- Krijal_Khadka_S385427/Week_1/week1HelloWorldMVC/obj/Debug/net10.0/week1HelloWorldMVC.AssemblyInfo.cs
+++ Krijal_Khadka_S385427/Week_1/week1HelloWorldMVC/obj/Debug/net10.0/week1HelloWorldMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("week1HelloWorldMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f4ecbf119b9507e17aff930fb1496f5565710e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b596dc25a154ce18b03289e2886935ae5af3a39")]
 [assembly: System.Reflection.AssemblyProductAttribute("week1HelloWorldMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("week1HelloWorldMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
