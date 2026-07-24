9f5d353807ebb255096ce7438be9a1ba812a3237
Regmi C. Mahesh <0xmahadev@gmail.com>
2026-07-24 16:22:06 +0930

    Add DB Context

--- MaheshChandraRegmi_S389242/Week_1/Projects/DbApp/Expense.cs
+++ MaheshChandraRegmi_S389242/Week_1/Projects/DbApp/Expense.cs
@@ -1,6 +1,9 @@
+using System.Linq.Expressions;
+
 namespace DbApp {
-    public class Expense {
-        public Guid Id { get; set; }
+    public class Expense
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Description { get; set; } = String.Empty;
         public decimal Amount { get; set; }
         public DateTime CreatedAt { get; set; }
@@ -8,4 +11,16 @@ namespace DbApp {
         public int CategoryId { get; set; }
         public Category? Category { get; set; }
     }
+
+    public interface IExpenseRepository
+    {
+        void Add(Expense expense);
+        List<Expense> GetAll();
+        List<Expense> GetWhere(Expression<Func<Expense, bool>> predicate);
+
+        bool Delete(Guid id);
+
+        decimal GetTotal();
+        decimal GetAverage();
+    };
 }
