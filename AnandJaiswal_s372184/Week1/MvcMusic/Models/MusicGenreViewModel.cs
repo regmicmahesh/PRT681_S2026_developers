032e212f9d3eac884eb18ca3db752803060a269b
anandjaiswal10101 <anandjaiswal10101@gmail.com>
2026-07-26 23:09:00 +0930

    model and Views

---
+++ AnandJaiswal_s372184/Week1/MvcMusic/Models/MusicGenreViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcMusic.Models;
+
+public class MusicGenreViewModel
+{
+    public List<Music>? MusicList { get; set; }
+    public SelectList? Genres { get; set; }
+    public string? MusicGenre { get; set; }
+    public string? SearchString { get; set; }
+}
