24ac7f19a373b1825ee8fc767c547618a56c83fe
badmiguel <badayos.miguel@gmail.com>
2026-07-27 15:53:33 +0930

    feat: create views

--- MiguelBadayos_S365958/Week_1/week1HelloWorldMVC/week1HelloWorldMVC/Controllers/HelloWorldController.cs
+++ MiguelBadayos_S365958/Week_1/week1HelloWorldMVC/week1HelloWorldMVC/Controllers/HelloWorldController.cs
@@ -1,18 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Text.Encodings.Web;
 
 namespace week1HelloWorldMVC.Controllers
 {
     public class HelloWorldController : Controller
     {
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
-        public string Welcome(string name, int ID = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
         }
     }
 }
