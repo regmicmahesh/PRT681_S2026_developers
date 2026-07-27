01a541c047ac4331c8401e88960f17796ff66e43
badmiguel <badayos.miguel@gmail.com>
2026-07-27 16:28:29 +0930

    chore: generate controller and view using scaffolding

--- MiguelBadayos_S365958/Week_1/week1HelloWorldMVC/week1HelloWorldMVC/Program.cs
+++ MiguelBadayos_S365958/Week_1/week1HelloWorldMVC/week1HelloWorldMVC/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("week1HelloWorldMVCContext") ?? throw new InvalidOperationException("Connection string 'week1HelloWorldMVCContext' not found.");
+
+builder.Services.AddDbContext<week1HelloWorldMVCContext>(options => options.UseSqlServer(connectionString));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
