bea5a701f5f87b4ba9e14d59439a7f13072de07b
badmiguel <badayos.miguel@gmail.com>
2026-07-27 19:27:13 +0930

    feat: add search bar for title

--- MiguelBadayos_S365958/Week_1/week1HelloWorldMVC/week1HelloWorldMVC/Controllers/MoviesController.cs
+++ MiguelBadayos_S365958/Week_1/week1HelloWorldMVC/week1HelloWorldMVC/Controllers/MoviesController.cs
@@ -12,10 +12,22 @@ public class MoviesController : Controller
         _context = context;
     }
 
-    // GET: MOVIES
-    public async Task<IActionResult> Index()    
+    public async Task<IActionResult> Index(string searchString)
     {
-        return View(await _context.Movie.ToListAsync());
+        if (_context.Movie == null)
+        {
+            return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+        }
+
+        var movies = from m in _context.Movie
+                     select m;
+
+        if (!String.IsNullOrEmpty(searchString))
+        {
+            movies = movies.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
+        }
+
+        return View(await movies.ToListAsync());
     }
 
     // GET: MOVIES/Details/5
