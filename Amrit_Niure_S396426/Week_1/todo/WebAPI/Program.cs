0d642a999e8961dbfbfe91c4b483e10f09025a69
Amrit Niure <niureamrit2@gmail.com>
2026-07-28 12:23:04 +0930

    Add Serilog configuration and project references for logging and domain layer integration

--- Amrit_Niure_S396426/Week_1/todo/WebAPI/Program.cs
+++ Amrit_Niure_S396426/Week_1/todo/WebAPI/Program.cs
@@ -1,6 +1,19 @@
+using Application;
+using Infrastructure;
+using Presentation;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services
+    .AddApplication()
+    .AddInfrastructure()
+    .AddPresentation();
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
 
 var app = builder.Build();
 
@@ -8,6 +21,8 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
