fea28cd2bcb6dfd213fa29d783b845563d1f67a3
Amrit Niure <niureamrit2@gmail.com>
2026-07-28 12:03:53 +0930

    Refactor application setup: add dependency injection for MediatR and FluentValidation, and clean up Program.cs by removing commented-out code and unused weather forecast endpoint.

--- Amrit_Niure_S396426/Week_1/todo/Application/DependencyInjection.cs
+++ Amrit_Niure_S396426/Week_1/todo/Application/DependencyInjection.cs
@@ -1,7 +1,16 @@
-﻿namespace Application
-{
-    public class Class1
-    {
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assembly = typeof(DependencyInjection).Assembly;
+            services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(assembly));
+            services.AddValidatorsFromAssembly(assembly);
+            return services;
+        }
     }
 }
