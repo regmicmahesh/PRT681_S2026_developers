82f1d6f9257ec57cc581877146a9bef6b5e6f71b
Amrit Niure <niureamrit2@gmail.com>
2026-07-29 20:15:15 +0930

    Implement IEquatable<Entity> interface and override equality methods in Entity class

--- Amrit_Niure_S396426/Week_2/gatherly/src/Gatherly.Domain/Primitives/Entity.cs
+++ Amrit_Niure_S396426/Week_2/gatherly/src/Gatherly.Domain/Primitives/Entity.cs
@@ -1,11 +1,51 @@
 ﻿namespace Gatherly.Domain.Primitives;
 
-public abstract class Entity
+public abstract class Entity : IEquatable<Entity>
 {
     protected Entity(Guid id)
     {
         Id = id;
     }
-    public Guid Id { get; init; }
+    public Guid Id { get; private init; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+        {
+            return false;
+        }
+
+        if (obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        if (obj is not Entity entity)
+        {
+            return false;
+        }
+
+        return Id == entity.Id;
+    }
+
+    public bool Equals(Entity? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        if (other.GetType() != GetType())
+        {
+            return false;
+        }
+
+        return Id == other.Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode() * 41;
+    }
 }
 
