10f864d235b82c4e95c5237d15d8f123ad374a82
badmiguel <badayos.miguel@gmail.com>
2026-07-31 08:54:33 +0930

    chore: initial project setup

---
+++ MiguelBadayos_S365958/Week_2/week2TheaterAdmin/week2TheaterAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace week2TheaterAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
