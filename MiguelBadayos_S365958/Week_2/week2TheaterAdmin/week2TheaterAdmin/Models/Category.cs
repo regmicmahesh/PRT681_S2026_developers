c3281cca973d5155de4e783e2867edff032430ac
badmiguel <badayos.miguel@gmail.com>
2026-07-31 09:48:03 +0930

    feat: create initial models

---
+++ MiguelBadayos_S365958/Week_2/week2TheaterAdmin/week2TheaterAdmin/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace week2TheaterAdmin.Models;
+
+public class Category
+{
+    public required int CategoryId { get; set; }
+    public required string CategoryCode { get; set; }
+    public required string CategoryName { get; set; }
+}
---
+++ MiguelBadayos_S365958/Week_2/week2TheaterAdmin/week2TheaterAdmin/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace week2TheaterAdmin.Models;
+
+public class Movie
+{
+    public required int MovieId { get; set; }
+    public required string MovieName { get; set; }
+    public required DateOnly ReleaseDate { get; set; }
+    public required string ContactEmailAddress { get; set; }
+    public required Language Language { get; set; }
+    public required Category Category { get; set; }
+}
