e6f97d54a2ae38bf908c787699d6b470d0e9e2a0
badmiguel <badayos.miguel@gmail.com>
2026-07-31 10:10:48 +0930

    fix: add movie to data context

--- MiguelBadayos_S365958/Week_2/week2TheaterAdmin/week2TheaterAdmin/week2TheaterAdminContext.cs
+++ MiguelBadayos_S365958/Week_2/week2TheaterAdmin/week2TheaterAdmin/week2TheaterAdminContext.cs
@@ -3,4 +3,5 @@ using Microsoft.EntityFrameworkCore;
 public class week2TheaterAdminContext(DbContextOptions<week2TheaterAdminContext> options) : DbContext(options)
 {
     public DbSet<week2TheaterAdmin.Models.Category> Category { get; set; } = default!;
+    public DbSet<week2TheaterAdmin.Models.Movie> Movie { get; set; } = default!;
 }
