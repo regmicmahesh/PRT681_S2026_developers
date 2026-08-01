5c450ebebf31920041766fe2b9c20fc2a034fd52
badmiguel <badayos.miguel@gmail.com>
2026-08-01 10:56:01 +0930

    feat: make validation more robust

--- MiguelBadayos_S365958/Week_2/week2TheaterAdmin/week2TheaterAdmin/Models/Movie.cs
+++ MiguelBadayos_S365958/Week_2/week2TheaterAdmin/week2TheaterAdmin/Models/Movie.cs
@@ -1,14 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
 namespace week2TheaterAdmin.Models;
 
 public class Movie
 {
     public int MovieId { get; set; }
+
+    [Required]
+    [StringLength(200)]
     public required string MovieName { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
     public required DateOnly ReleaseDate { get; set; }
+
+    [Required]
+    [StringLength(150)]
     public required string Director { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [StringLength(256)]
+    [Display(Name = "Contact Email Address")]
     public required string ContactEmailAddress { get; set; }
+
+    [Required]
     public required Language Language { get; set; }
 
+    [Required]
     public int CategoryId { get; set; }
+
+    [ValidateNever]
     public required Category Category { get; set; }
 }
