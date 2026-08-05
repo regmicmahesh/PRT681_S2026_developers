dcd70764010287dcfa7949b99ddf4d6589a085f1
Dipesh Wagle <deepacewagle@gmail.com>
2026-08-05 13:57:24 +0930

    Applied crud functionality using a dapper for .NET core API and did the database connectivity

--- Dipesh_wagle_S394745/Week_3/SimpleCrudAppAPI/SimpleCrudAppAPI/Program.cs
+++ Dipesh_wagle_S394745/Week_3/SimpleCrudAppAPI/SimpleCrudAppAPI/Program.cs
@@ -1,3 +1,5 @@
+using SimpleCrudAppAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
+
+// Add Dapper repository for Product data access
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
