2e0756362568e8da41f118eaa76b7cb5dd8446f3
Amrit Niure <niureamrit2@gmail.com>
2026-08-06 15:55:40 +0930

    feat: wire up DbContext DI, CORS policy for Angular dev server, and OpenAPI
    Pin SQLitePCLRaw.lib.e_sqlite3 to a patched version to clear a known
    high-severity advisory in the default transitive resolution.

--- Amrit_Niure_S396426/Week 3/todo/backend/TodoApi/Program.cs
+++ Amrit_Niure_S396426/Week 3/todo/backend/TodoApi/Program.cs
@@ -1,6 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+using TodoApi.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
+
+const string AngularDevCors = "AngularDevCors";
+
+builder.Services.AddDbContext<TodoDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(AngularDevCors, policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseCors(AngularDevCors);
+
+app.MapTodoEndpoints();
 
 app.Run();
