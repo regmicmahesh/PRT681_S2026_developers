2ac9370902500156f015e7b5f7d84511bccb9d8c
Deepjan Thapaliya <80693014+couch-cushion@users.noreply.github.com>
2026-08-14 09:43:23 +0930

    Models are created

---
+++ DeepjanThapaliya_S395373/Week_3/Practice/ClassPractice/StudentResultSystem/StudentResultSystem/Models/AssignmentAssessment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentResultSystem.Models
+{
+    internal class AssignmentAssessment:Assessment
+    {
+        public decimal ObtainedMark { get; private set; }
+
+        public AssignmentAssessment(string name, decimal mark) : base(name)
+        {
+            ObtainedMark = mark;
+        }
+
+        public override decimal Score()
+        {
+            return ObtainedMark;
+        }
+    }
+}
---
+++ DeepjanThapaliya_S395373/Week_3/Practice/ClassPractice/StudentResultSystem/StudentResultSystem/Models/ExamAssessment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentResultSystem.Models
+{
+    internal class ExamAssessment:Assessment
+    {
+        public decimal obtainedMark { get; private set; }
+
+        public ExamAssessment(string name, decimal mark): base(name)
+        {
+            obtainedMark = mark;
+        }
+
+        public override decimal Score()
+        {
+            return obtainedMark;
+        }
+    }
+}
