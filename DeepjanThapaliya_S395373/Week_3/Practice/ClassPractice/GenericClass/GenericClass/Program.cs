d914e35002ee1b7418e891f93d200e8ff5e9869e
Deepjan Thapaliya <80693014+couch-cushion@users.noreply.github.com>
2026-08-15 16:30:53 +0930

    Initial Generic class done.

--- DeepjanThapaliya_S395373/Week_3/Practice/ClassPractice/GenericClass/GenericClass/Program.cs
+++ DeepjanThapaliya_S395373/Week_3/Practice/ClassPractice/GenericClass/GenericClass/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+Box<string> nameBox = new ("Deepjan");
+Box<int> ageBox = new (28);
+Box<decimal> salaryBox = new (700m);
+
+nameBox.ShowValue();
+ageBox.ShowValue();
+salaryBox.ShowValue();
+
+public class Box<T>
+{
+    public T Value { get; private set; }
+
+    public Box(T value)
+    {
+        Value = value;
+    }
+
+    public void ShowValue()
+    {
+        Console.WriteLine($"Stored value: {Value}");
+    }
+}
+
