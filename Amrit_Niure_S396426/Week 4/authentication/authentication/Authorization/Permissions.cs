8f465e7843dc74cf38b2d8406acc6c889af06200
Amrit Niure <niureamrit2@gmail.com>
2026-08-15 19:56:08 +0930

    feat(authentication): add CustomClaimTypes and Permissions classes for authorization management

---
+++ Amrit_Niure_S396426/Week 4/authentication/authentication/Authorization/Permissions.cs
@@ -0,0 +1,10 @@
+﻿namespace authentication.Authorization
+{
+    public static class Permissions
+    {
+        public const string UsersRead = "user:read";
+        public const string UsersUpdate = "user:update";
+        public const string UsersDelete = "user:delete";
+    }
+}
+
