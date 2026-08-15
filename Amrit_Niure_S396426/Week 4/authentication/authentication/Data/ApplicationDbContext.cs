191803fabeec62c6b6b1c62d72e911da1366a229
Amrit Niure <niureamrit2@gmail.com>
2026-08-15 14:09:09 +0930

    feat(authentication): add ApplicationDbContext and ApplicationUser classes for database integration

---
+++ Amrit_Niure_S396426/Week 4/authentication/authentication/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace authentication.Data;
+
+public sealed class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
+
+
+
+
+public sealed class ApplicationUser : IdentityUser
+{
+    public bool EnableNotifications { get; set; }
+    public string Initials { get; set; } = string.Empty;
+}
