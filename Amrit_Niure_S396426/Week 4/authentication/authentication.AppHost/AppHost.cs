59c28b75335c51f27195eb48b73dbe2c716b34ec
Amrit Niure <niureamrit2@gmail.com>
2026-08-15 12:13:03 +0930

    feat(authentication): configure PostgreSQL database connection and update project structure

--- Amrit_Niure_S396426/Week 4/authentication/authentication.AppHost/AppHost.cs
+++ Amrit_Niure_S396426/Week 4/authentication/authentication.AppHost/AppHost.cs
@@ -1,7 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var usersDb = builder.AddPostgres("database")
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithHostPort(5432)
+    .AddDatabase("users-db");
 
-
-builder.AddProject<Projects.authentication>("authentication");
+builder.AddProject<Projects.authentication>("authentication-api")
+    .WithReference(usersDb)
+    .WaitFor(usersDb);
 
 builder.Build().Run();
