9e5f0b10739c22090b2d233713798d0444d6fa34
khadkakrijal <khadkakrijal36@gmail.com>
2026-08-15 12:54:15 +0930

    Configure context dependency injection

--- Krijal_Khadka_S385427/Week_4/BookStoreApp/Program.cs
+++ Krijal_Khadka_S385427/Week_4/BookStoreApp/Program.cs
@@ -1,7 +1,16 @@
+using BookStoreApp.DAL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add MVC services to the container.
 builder.Services.AddControllersWithViews();
+
+// Register the Entity Framework Core context.
+builder.Services.AddDbContext<EfBookStoreContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString(
+            "EfDbconnectionString")));
 
 var app = builder.Build();
 
@@ -9,7 +18,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -24,6 +32,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
