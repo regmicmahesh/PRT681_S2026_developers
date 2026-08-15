334a27774a765dc6cdf220039a7d52f3feb59098
Amrit Niure <niureamrit2@gmail.com>
2026-08-15 23:27:39 +0930

    feat(authorization): add PermissionAuthorizationRequirement class for permission-based authorization

---
+++ Amrit_Niure_S396426/Week 4/authentication/authentication/Authorization/PermissionAuthorizationRequirement.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Authorization;
+
+namespace authentication.Authorization;
+
+public class PermissionAuthorizationRequirement(params string[] allowedPermissions)
+    : AuthorizationHandler<PermissionAuthorizationRequirement>, IAuthorizationRequirement
+{
+    public string[] AllowedPermissions { get; } = allowedPermissions;
+
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionAuthorizationRequirement requirement)
+    {
+        foreach (var permission in requirement.AllowedPermissions)
+        {
+            bool found = context.User.FindFirst(c =>
+                c.Type == CustomClaimTypes.Permission &&
+                c.Value == permission) is not null;
+
+            if (found)
+            {
+                context.Succeed(requirement);
+                break;
+            }
+        }
+        return Task.CompletedTask;
+    }
+}
