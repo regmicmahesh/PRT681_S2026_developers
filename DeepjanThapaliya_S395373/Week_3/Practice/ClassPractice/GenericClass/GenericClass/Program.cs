85978013e13176c3fe2eccd9151f33a19a3794ba
Deepjan Thapaliya <80693014+couch-cushion@users.noreply.github.com>
2026-08-15 16:40:05 +0930

    More Generic class practise.

--- DeepjanThapaliya_S395373/Week_3/Practice/ClassPractice/GenericClass/GenericClass/Program.cs
+++ DeepjanThapaliya_S395373/Week_3/Practice/ClassPractice/GenericClass/GenericClass/Program.cs
@@ -1,24 +1,37 @@
 ﻿
-Box<string> nameBox = new ("Deepjan");
-Box<int> ageBox = new (28);
-Box<decimal> salaryBox = new (700m);
-
-nameBox.ShowValue();
-ageBox.ShowValue();
-salaryBox.ShowValue();
-
-public class Box<T>
+PrintItem("Deepjan");
+PrintItem(28);
+PrintItem(700m);
+PrintItem(true);
+static void PrintItem <T>( T item)
 {
-    public T Value { get; private set; }
-
-    public Box(T value)
-    {
-        Value = value;
-    }
-
-    public void ShowValue()
-    {
-        Console.WriteLine($"Stored value: {Value}");
-    }
+    Console.WriteLine($"Item: {item}");
 }
+
+
+
+
+
+//Box<string> nameBox = new ("Deepjan");
+//Box<int> ageBox = new (28);
+//Box<decimal> salaryBox = new (700m);
+
+//nameBox.ShowValue();
+//ageBox.ShowValue();
+//salaryBox.ShowValue();
+
+//public class Box<T>
+//{
+//    public T Value { get; private set; }
+
+//    public Box(T value)
+//    {
+//        Value = value;
+//    }
+
+//    public void ShowValue()
+//    {
+//        Console.WriteLine($"Stored value: {Value}");
+//    }
+//}
 
