b42ad8dec876915c8ca1ef06ba11f1db4df3f1a3
khadkakrijal <khadkakrijal36@gmail.com>
2026-08-15 12:51:07 +0930

    Creating context

---
+++ Krijal_Khadka_S385427/Week_4/BookStoreApp/DAL/EfBookStoreContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApp.DAL;
+
+public class EfBookStoreContext : DbContext
+{
+    public EfBookStoreContext(
+        DbContextOptions<EfBookStoreContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Book> Books { get; set; }
+
+    public DbSet<Printer> Printers { get; set; }
+}
