69d94bcb66b72027cf6e80362f8559f57b87f1a6
Deepjan Thapaliya <80693014+couch-cushion@users.noreply.github.com>
2026-08-16 14:23:20 +0930

    New stroe generic list.

--- DeepjanThapaliya_S395373/Week_3/Practice/ClassPractice/SimpleStore/SimpleStore/Program.cs
+++ DeepjanThapaliya_S395373/Week_3/Practice/ClassPractice/SimpleStore/SimpleStore/Program.cs
@@ -1,1 +1,39 @@
-﻿Console.WriteLine("Hello, World!");
+﻿SimpleStore<string> nameStore = new SimpleStore<string>();
+
+nameStore.AddItem("Deepjan");
+nameStore.AddItem("Alex");
+nameStore.AddItem("Sarah");
+
+
+nameStore.PrintAllItems();
+Console.WriteLine();
+SimpleStore<int> ageStore = new SimpleStore<int>();
+ageStore.AddItem(28);
+ageStore.AddItem(26);
+ageStore.AddItem(21);
+
+ageStore.PrintAllItems();
+
+public class SimpleStore<T>
+{
+    private List<T> _items =new List<T>();
+
+   
+
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+    public List<T> GetAllItems()
+    {
+        return _items;
+    }
+
+    public void PrintAllItems()
+    {
+        foreach(T item in _items)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
