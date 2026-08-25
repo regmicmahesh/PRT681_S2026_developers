18e63ff505fc0695f64cce220fd6c6d9ee5babf5
Deepjan Thapaliya <80693014+couch-cushion@users.noreply.github.com>
2026-08-25 11:49:22 +0930

    LINQ more.

--- DeepjanThapaliya_S395373/Week_4AndPlus/Practice/ClassPractice/LINQFirstOrDefault/LINQFirstOrDefault/Models/Product.cs
+++ DeepjanThapaliya_S395373/Week_4AndPlus/Practice/ClassPractice/LINQFirstOrDefault/LINQFirstOrDefault/Models/Product.cs
@@ -9,13 +9,17 @@ namespace LINQFirstOrDefault.Models
     {
         public int Id { get; private set; }
         public string ProductName { get; private set; }
+        public string Category { get; private set; }
         public decimal Price { get; private set; }
+        public int StockQuantity { get; private set; }
 
-        public Product(int id, string productName, decimal price)
+        public Product(int id, string productName,string category, decimal price, int stockQuantity)
         {
             Id = id;
             ProductName = productName;
+            Category = category;
             Price = price;
+            StockQuantity = stockQuantity;
         }
 
         public override string ToString()
--- DeepjanThapaliya_S395373/Week_4AndPlus/Practice/ClassPractice/LINQFirstOrDefault/LINQFirstOrDefault/Program.cs
+++ DeepjanThapaliya_S395373/Week_4AndPlus/Practice/ClassPractice/LINQFirstOrDefault/LINQFirstOrDefault/Program.cs
@@ -1,12 +1,19 @@
 ﻿using LINQFirstOrDefault.Models;
+using static System.Net.WebRequestMethods;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-List<Product> products = new List<Product>();
+List<Product> products = new List<Product>
+{
+    new Product(1, "Laptop", "Electronics", 1500m, 5),
+    new Product(2, "Mouse", "Electronics", 25m, 50),
+    new Product(3, "Keyboard", "Electronics", 80m, 30),
+    new Product(4, "Office Chair", "Furniture", 250m, 10),
+    new Product(5, "Desk", "Furniture", 400m, 7),
+    new Product(6, "Notebook", "Stationery", 5m, 100),
+    new Product(7, "Pen", "Stationery", 2m, 200)
+};
 
-products.Add(new Product(1, "Laptop", 1500m));
-products.Add(new Product(2, "Mouse", 25m));
-products.Add(new Product(3, "Keyboard", 80m));
-
-Product? foundProduct = products.FirstOrDefault(product => product.Id ==2);//lambda expression
+Product? foundProduct = products.FirstOrDefault(product => product.Id ==2);//lambda expression. Returns first true.
 if (foundProduct == null)
 {
     Console.WriteLine("Product not found.");
@@ -16,3 +23,23 @@ else
     Console.WriteLine($"\nProduct Found: {foundProduct}\n");
 }
 
+List<Product> expensiveProducts = products.Where(product => product.Price >= 100m).ToList();
+
+
+Console.WriteLine("Expensive products:");
+Console.WriteLine();
+
+foreach (Product product in expensiveProducts)
+{
+    Console.WriteLine(product);
+}
+
+
+//Where() = filter many items
+//FirstOrDefault() = find one item
+//OrderBy() = sort items
+//Select() = transform items
+//Count() = count items
+//Sum() = total values
+//Any() = check if at least one exists
+//foreach = do an action for each item
