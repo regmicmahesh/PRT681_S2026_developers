ad0d2b50d2a5ea3a570baeca52c718ceb8fe0fb7
Deepjan Thapaliya <80693014+couch-cushion@users.noreply.github.com>
2026-08-25 10:58:35 +0930

    First LINQ used for search.

---
+++ DeepjanThapaliya_S395373/Week_4AndPlus/Practice/ClassPractice/LINQFirstOrDefault/LINQFirstOrDefault/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQFirstOrDefault.Models
+{
+    public class Product
+    {
+        public int Id { get; private set; }
+        public string ProductName { get; private set; }
+        public decimal Price { get; private set; }
+
+        public Product(int id, string productName, decimal price)
+        {
+            Id = id;
+            ProductName = productName;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} - {ProductName} - ${Price}";
+        }
+    }
+}
---
+++ DeepjanThapaliya_S395373/Week_4AndPlus/Practice/ClassPractice/LINQFirstOrDefault/LINQFirstOrDefault/Program.cs
@@ -0,0 +1,18 @@
+﻿using LINQFirstOrDefault.Models;
+
+List<Product> products = new List<Product>();
+
+products.Add(new Product(1, "Laptop", 1500m));
+products.Add(new Product(2, "Mouse", 25m));
+products.Add(new Product(3, "Keyboard", 80m));
+
+Product? foundProduct = products.FirstOrDefault(product => product.Id ==2);//lambda expression
+if (foundProduct == null)
+{
+    Console.WriteLine("Product not found.");
+}
+else
+{
+    Console.WriteLine($"\nProduct Found: {foundProduct}\n");
+}
+
