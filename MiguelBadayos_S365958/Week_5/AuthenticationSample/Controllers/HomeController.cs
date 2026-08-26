17700179e6da858f2c0a2fb9b00e93c18fc2b6b8
badmiguel <badayos.miguel@gmail.com>
2026-08-26 09:46:41 +0930

    feat: require authentication to view privacy page

--- MiguelBadayos_S365958/Week_5/AuthenticationSample/Controllers/HomeController.cs
+++ MiguelBadayos_S365958/Week_5/AuthenticationSample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using AuthenticationSample.Models;
 
@@ -11,6 +12,7 @@ public class HomeController : Controller
         return View();
     }
 
+    [Authorize]
     public IActionResult Privacy()
     {
         return View();
